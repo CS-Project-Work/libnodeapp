6a90fd7ad32ad6f85b046342e42f9b7a13e551fe
Pranav Sastry <pranava.sri@gmail.com>
2021-06-21 11:10:18 +0530

    Added JS rendering to Stock Manager

--- Departmental-Digital-Inventory-main/nodeapp/asem.js
+++ Departmental-Digital-Inventory-main/nodeapp/asem.js
@@ -1,30 +1,31 @@
-function Cancel(i) 
-{
-var togbutton = document.getElementsByClassName("btn");
-
-if(togbutton[i].innerHTML === "Issue") {
-  togbutton[i].innerHTML = "Cancel";
-}
-else
+function Cancel(i)
   {
-    togbutton[i].innerHTML = "Issue";
+  var togbutton = document.getElementsByClassName("btn");
+
+  if(togbutton[i].innerHTML === "Issue") {
+    togbutton[i].innerHTML = "Cancel";
   }
+  else
+    {
+      togbutton[i].innerHTML = "Issue";
+    }
 
 }
+
 
 function get_count(j){
-var button_var = document.getElementsByClassName("btn");
-var count_var = parseInt(document.getElementById("count_val").innerHTML);
-if(count_var<7&&button_var[j].innerHTML==="Issue"){
-  count_var++;
-  document.getElementById("count_val").innerHTML = count_var.toString();
-}
-else if(button_var[j].innerHTML==="Cancel"&&count_var>0){
-  count_var--;
-  document.getElementById("count_val").innerHTML = count_var.toString();
-}
-else{
-  alert("You have reached the maximum amount of books that are issued to an admin!");
-  Cancel(j);
-}
+  var button_var = document.getElementsByClassName("btn");
+  var count_var = parseInt(document.getElementById("count_val").innerHTML);
+  if(count_var<7&&button_var[j].innerHTML==="Issue"){
+    count_var++;
+    document.getElementById("count_val").innerHTML = count_var.toString();
+  }
+  else if(button_var[j].innerHTML==="Cancel"&&count_var>0){
+    count_var--;
+    document.getElementById("count_val").innerHTML = count_var.toString();
+  }
+  else{
+    alert("You have reached the maximum amount of books that are issued to an admin!");
+    Cancel(j);
+  }
 }
--- Departmental-Digital-Inventory-main/nodeapp/sm_3.js
+++ Departmental-Digital-Inventory-main/nodeapp/sm_3.js
@@ -1,17 +1,82 @@
-function inc(i){
-    var stock =  parseInt(document.getElementById("stock"+i).innerHTML);
-    var final_ans = (stock+1).toString(); 
-    document.getElementById("stock"+i).innerHTML = final_ans; 
+function inc(formatted_subject_name, i){
+    var stock =  parseInt(document.getElementById(formatted_subject_name+"_stock_"+i).innerHTML);
+    var final_ans = (stock+1).toString();
+    document.getElementById(formatted_subject_name+"_stock_"+i).innerHTML = final_ans;
    }
-   
-   function dec(i){
-    var stock =  parseInt(document.getElementById("stock"+i).innerHTML);
-    if(stock<=0){
-      var final_ans = 0;
-    }
-    else{
-      var final_ans = (stock-1); 
-    }
-    document.getElementById("stock"+i).innerHTML = final_ans.toString(); 
-   }
-   
+
+
+function dec(formatted_subject_name, i){
+  var stock =  parseInt(document.getElementById(formatted_subject_name+"_stock_"+i).innerHTML);
+  if(stock<=0){
+    var final_ans = 0;
+  }
+  else{
+    var final_ans = (stock-1);
+  }
+  document.getElementById(formatted_subject_name+"_stock_"+i).innerHTML = final_ans.toString();
+}
+
+
+function generate_headers(headers){
+  var header_objs = [];
+  for(header in headers){
+    var header_obj = document.createElement('th');
+    header_obj.innerHTML = header;
+    header_objs.push(header_obj);
+  }
+  return header_objs;
+}
+
+
+function format_subject_name(subject_name){
+  var lower_case = subject_name.toLowerCase();
+  var join_with_underscore = lower_case.replace(" ", "_");
+  return join_with_underscore;
+}
+
+
+function generate_tables(num, subject_name, data){
+  var table = document.createElement('table');
+  var headers = ['Book Title', 'Author', 'Stocks left', 'Add stocks', 'Delete stocks'];
+  var header_objs = generate_headers(headers);
+  var header_row = document.createElement('tr');
+  for(header_obj in header_objs){
+    header_row.append(header_obj);
+  }
+  table.append(header_row);
+  for(i=0;i<num;i++){
+    var table_row = document.createElement('tr');
+    var formatted_subject_name = format_subject_name(subject_name);
+    table_row.id = formatted_subject_name + "_" + i.toString();
+
+    var title_entry = document.createElement('td');
+    title_entry.innerHTML = data[i]['title'];
+    table_row.append(title_entry);
+
+    var author_entry = document.createElement('td');
+    author_entry.innerHTML = data[i]['author'];
+    table_row.append(author_entry);
+
+    var stocks_left_entry = document.createElement('td');
+    stocks_left_entry.id = formatted_subject_name + "_stock_" + i.toString();
+    stocks_left_entry.innerHTML = data[i]['stocks_left'];
+    table_row.append(stocks_left_entry);
+
+    var add_stocks_entry = document.createElement('td');
+    var add_stocks_button = document.createElement('button');
+    add_stocks_button.className = "button";
+    add_stocks_button.innerHTML = "Add";
+    add_stocks_button.onclick = inc(formatted_subject_name, i);
+    add_stocks_entry.append(add_stocks_button);
+    table_row.append(add_stocks_entry);
+
+    var del_stocks_entry = document.createElement('td');
+    var del_stocks_button = document.createElement('button');
+    del_stocks_button.className = "button1";
+    del_stocks_button.innerHTML = "Delete";
+    del_stocks_button.onclick = dec(formatted_subject_name, i);
+    del_stocks_entry.append(del_stocks_button);
+    table_row.append(del_stocks_entry);
+    table.append(table_row);
+  }
+}
