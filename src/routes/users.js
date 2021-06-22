a9ae0590243e0a5390c81742260f814d635eea8a
Tarun-1BM19CV118 <72756481+Tarun-1BM19CV118@users.noreply.github.com>
2021-06-22 11:09:03 +0530

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var router14 = express.Router();
+var db = require('../database');
+// another routes also appear here
+// this script to fetch data from MySQL databse table
+router14.get('/user-list', function(req, res, next) {
+    var sql = 'SELECT * FROM stud_registration';
+    db.query(sql, function(err, data, fields) {
+        if (err) throw err;
+        res.render('user-list', { title: 'User List', userData: data });
+    });
 });
-
-module.exports = router;
+module.exports = router14;
